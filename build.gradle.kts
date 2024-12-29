b231f54c5da6aaafee56a6ee7186c756f4a2232f
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-12-29 20:46:28 +0100

    feat: add publishing method in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,17 @@ import org.gradle.kotlin.dsl.invoke
 
 plugins {
     alias(libs.plugins.kotlin)
-    alias(libs.plugins.shadow)
+    alias(libs.plugins.sonatype.central.portal.publisher)
+    `maven-publish`
 }
+
+val baseVersion = "0.0.1"
+val commitHash = System.getenv("COMMIT_HASH")
+val snapshotVersion = "${baseVersion}-dev.$commitHash"
 
 allprojects {
     group = "app.simplecloud.plugin"
-    version = "1.0-SNAPSHOT"
+    version = if (commitHash != null) snapshotVersion else baseVersion
 
     repositories {
         mavenCentral()
@@ -21,13 +26,10 @@ allprojects {
 
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
-    apply(plugin = "com.gradleup.shadow")
 
     dependencies {
-        testImplementation(rootProject.libs.kotlinTest)
-        implementation(rootProject.libs.kotlinJvm)
-        implementation(rootProject.libs.bundles.simpleCloudController)
-
+        compileOnly(rootProject.libs.kotlinJvm)
+        compileOnly(rootProject.libs.bundles.simpleCloudController)
         compileOnly(rootProject.libs.bundles.adventure)
     }
 
@@ -36,6 +38,61 @@ subprojects {
     }
 }
 
-tasks.test {
-    useJUnitPlatform()
+publishing {
+    repositories {
+        maven {
+            name = "simplecloud"
+            url = uri("https://repo.simplecloud.app/snapshots/")
+            credentials {
+                username = System.getenv("SIMPLECLOUD_USERNAME")?: (project.findProperty("simplecloudUsername") as? String)
+                password = System.getenv("SIMPLECLOUD_PASSWORD")?: (project.findProperty("simplecloudPassword") as? String)
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+}
+
+centralPortal {
+    name = project.name
+
+    username = project.findProperty("sonatypeUsername") as? String
+    password = project.findProperty("sonatypePassword") as? String
+
+    pom {
+        name.set("SimpleCloud Plugin API")
+        description.set("Commonly used classes among plugins")
+        url.set("https://github.com/theSimpleCloud/plugin-api")
+        developers {
+            developer {
+                id.set("MrManHD")
+            }
+        }
+        licenses {
+            license {
+                name.set("Apache-2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        scm {
+            url.set("https://github.com/theSimpleCloud/plugin-api.git")
+            connection.set("git:git@github.com:theSimpleCloud/plugin-api.git")
+        }
+    }
+}
+
+signing {
+    if (commitHash != null) {
+        return@signing
+    }
+
+    sign(publishing.publications)
+    useGpgCmd()
 }
