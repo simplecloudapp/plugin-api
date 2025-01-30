2976b9dfcdac34ad8c581899a0cb25c8fa295984
PrexorJustin <dev@scharbau.me>
2025-01-31 00:20:36 +0100

    feat: added a more abstract DirectoryRepository implementation

---
+++ plugin-shared/src/main/kotlin/app/simplecloud/plugin/api/shared/config/repository/handler/YamlFileHandler.kt
@@ -0,0 +1,48 @@
+package app.simplecloud.plugin.api.shared.config.repository.handler
+
+import app.simplecloud.plugin.api.shared.repository.GenericEnumSerializer
+import org.spongepowered.configurate.kotlin.objectMapperFactory
+import org.spongepowered.configurate.yaml.NodeStyle
+import org.spongepowered.configurate.yaml.YamlConfigurationLoader
+import java.io.File
+
+class YamlFileHandler<T : Any>(
+    private val clazz: Class<T>
+) : FileHandler<T> {
+
+    override val fileExtension: String = ".yml"
+
+    private val loaders = mutableMapOf<File, YamlConfigurationLoader>()
+
+    private fun getOrCreateLoader(file: File): YamlConfigurationLoader {
+        return loaders.getOrPut(file) {
+            YamlConfigurationLoader.builder()
+                .path(file.toPath())
+                .nodeStyle(NodeStyle.BLOCK)
+                .defaultOptions { options ->
+                    options.serializers { builder ->
+                        builder.registerAnnotatedObjects(objectMapperFactory())
+                        builder.register(Enum::class.java, GenericEnumSerializer)
+                    }
+                }
+                .build()
+        }
+    }
+
+    override fun load(file: File): T? {
+        return try {
+            val loader = getOrCreateLoader(file)
+            val node = loader.load()
+            node.get(clazz)
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    override fun save(file: File, entity: T) {
+        val loader = getOrCreateLoader(file)
+        val node = loader.createNode()
+        node.set(clazz, entity)
+        loader.save(node)
+    }
+}
