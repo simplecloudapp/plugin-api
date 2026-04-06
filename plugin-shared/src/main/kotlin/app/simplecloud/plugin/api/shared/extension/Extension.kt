95a0407305117f874d75820789285a475c5fd6d7
niklas.nieberler <niklas.nieberler@outlook.de>
2026-04-06 14:07:04 +0200

    feat: add PersistentServerPlaceholder support

--- plugin-shared/src/main/kotlin/app/simplecloud/plugin/api/shared/extension/Extension.kt
+++ plugin-shared/src/main/kotlin/app/simplecloud/plugin/api/shared/extension/Extension.kt
@@ -1,6 +1,7 @@
 package app.simplecloud.plugin.api.shared.extension
 
 import app.simplecloud.api.group.Group
+import app.simplecloud.api.persistentserver.PersistentServer
 import app.simplecloud.api.server.Server
 import app.simplecloud.plugin.api.shared.placeholder.PlaceholderProvider
 import app.simplecloud.plugin.api.shared.placeholder.argument.ArgumentsResolver
@@ -32,4 +33,12 @@ suspend fun String.appendToComponent(
     vararg argumentsResolver: ArgumentsResolver
 ): Component {
     return PlaceholderProvider.groupPlaceholderProvider.append(group, this, prefix, *argumentsResolver)
+}
+
+suspend fun String.appendToComponent(
+    persistentServer: PersistentServer,
+    prefix: String? = null,
+    vararg argumentsResolver: ArgumentsResolver
+): Component {
+    return PlaceholderProvider.persistentServerPlaceholderProvider.append(persistentServer, this, prefix, *argumentsResolver)
 }
